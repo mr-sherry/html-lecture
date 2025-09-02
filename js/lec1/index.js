d5250c6d4080603d1a06095b3130bc5c591d68fb
mr-sherry <mr.sherry6967@gmail.com>
2025-09-02 13:05:34 +0500

    js first lecture

---
+++ js/lec1/index.js
@@ -0,0 +1,44 @@
+console.log(123);
+
+// veriables
+// 1: var
+// 2: const
+// 3: let
+
+var name = "5687sdjdsa/..";
+console.log(name);
+
+name = "kaka";
+console.log(name);
+
+const age = 123234809324;
+console.log(age);
+
+// age = 30;
+// console.log(age);
+
+let address = "10 floor";
+console.log(address);
+
+address = "20 floor";
+console.log(address);
+
+// -+/*
+
+let a = 92;
+let b = 78;
+
+let c = a - b * 9;
+console.log("jdsksdljka", c);
+
+console.log(10 - 8);
+console.log(10 * 8);
+console.log(10 / 8);
+console.log(10 + 8);
+
+// let switch = false;
+// let switch1 = true;
+let hello = null;
+console.log(hello);
+
+console.log(typeof a);
