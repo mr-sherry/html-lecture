4e5703556cc61802ae03463b60c4178fb718493c
mr-sherry <mr.sherry6967@gmail.com>
2025-09-03 13:19:58 +0500

    operators

---
+++ js/lec2/index.js
@@ -0,0 +1,41 @@
+let a = 2;
+let b = 8;
+let c = a % b;
+
+let d = a ** b;
+
+let increment = c;
+console.log(c++);
+
+console.log(--d);
+
+let fName = "ahmed";
+let lName = "raza";
+
+let fullname = "welcome to dashboard:" + fName + lName;
+
+let fullname1 = `welcome ${increment} to dashboard: ${fName} ${lName}`;
+
+console.log(fullname);
+console.log(fullname1);
+
+let num = 10;
+
+// num = 20 + num;
+// num += 20;
+
+num **= 2;
+
+console.log(num);
+
+// num=20;
+
+let value1 = "10";
+let value2 = 100;
+
+console.log(typeof value1);
+console.log(typeof value2);
+
+console.log(value1 === value2);
+console.log(value1 !== value2);
+console.log(value1 < value2);
