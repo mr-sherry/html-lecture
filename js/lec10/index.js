6c4cb2a4a639b059e9b4e4613d98f7fe097b88cc
mr.sherry <mr.sherry6967@gmail.com>
2025-09-22 08:52:09 +0100

    for...of loop todo task

---
+++ js/lec10/index.js
@@ -0,0 +1,30 @@
+let array = Array.from('abc vbnn 112');
+// let array = Array.from('a', 'b', 'c');
+
+console.log(array);
+
+let array1 = Array.of('kaka', 'mama', 'papa', 223);
+console.log(array1);
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let index = 0; index < data.length; index++) {
+//     console.log(data[index]);
+
+// }
+
+for (const num of data) {
+  console.log(num);
+}
+for (const num of data) {
+  console.log(num * num);
+}
+
+let todos = ['clean car', ''];
+
+function add(text) {}
+function deleteTodo(index, text) {}
+function edit(index) {}
+
+console.log(todos);
+
+add("jajaja")
